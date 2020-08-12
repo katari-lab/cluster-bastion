b712548ba906286c9279ae52952711d7fc0d40ae
gregory <gcvalderrama@hotmail.com>
2020-08-12 11:00:56 -0500

    add

--- container/build.sh
+++ container/build.sh
@@ -1,4 +1,4 @@
 docker-compose build
 
-docker tag katari/bastion localhost:32000/katari/bastion
-docker push localhost:32000/katari/bastion
+docker tag katari/bastion localhost:32000/katarilab/bastion
+docker push localhost:32000/katarilab/bastion
